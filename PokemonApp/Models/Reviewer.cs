11bf05c527d79bdfafdd540a39b238ebc0bafc77
MadihaGul <MadihaGul@MADIHA>
2023-04-26 14:26:18 +0200

    Add project files.

---
+++ PokemonApp/Models/Reviewer.cs
@@ -0,0 +1,12 @@
+﻿namespace PokemonApp.Models
+{
+    public class Reviewer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+
+        public ICollection<Review> Reviews { get; set; }
+
+    }
+}
